5cc5f11506a43cd6d5e6e1ea4a0089b92d913a68
Nativ Maor <nativ.maor@gmail.com>
2021-04-14 14:45:41 +0000

    ex3 seems to be done

--- ex3.asm
+++ ex3.asm
@@ -3,3 +3,31 @@
 .section .text
 _start:
 #your code here
+mov $0,%rax # rax == sum = 0
+mov $0,%rcx # rcx == count = 0
+mov $arr, %rsi
+
+cmp $0,(%rsi)
+je avg_is_zero
+
+loop:
+movsxd (%rsi),%rdx
+add %rdx,%rax
+inc %rcx
+add $4,%rsi
+cmp $0,(%rsi)
+jne loop
+
+xor %rdx,%rdx
+div %rcx
+mov %eax, avg
+jmp end
+
+avg_is_zero:
+movl $0,avg
+
+end:
+
+
+
+
---
+++ ex3_sasm.asm
@@ -0,0 +1,30 @@
+.global main 
+.section .data
+arr: .int 1,2,3,4,5,6,7,8,9,0
+.section .text
+main:
+#your code here
+movq $0,%rax # rax == sum = 0
+movq $0,%rcx # rcx == count = 0
+movq $arr, %rsi
+
+xor %rdx, %rdx
+
+cmp $0,(%rsi)
+je end
+
+loop:
+movsxd (%rsi),%rdx
+add %rdx,%rax
+inc %rcx
+add $4,%rsi
+cmp $0,(%rsi)
+jne loop
+
+end:
+xor %rdx,%rdx
+divq %rcx
+inc %rcx # for debugging
+
+
+
